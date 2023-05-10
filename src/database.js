095ebc47cef5849f9061a4d6785f4ed561828c7a
Luis-Bayro <124312386+Luis-Bayro@users.noreply.github.com>
2023-05-10 15:54:11 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://0.0.0.0:27017/Alumnos';
+const URI = 'mongodb://0.0.0.0/Alumnos';
 mongoose.connect(URI)
     .then(db=>console.log('db is connected'))
     .catch(err => console.error(err));
